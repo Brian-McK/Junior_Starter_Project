30c306eea69fb43e15ddcfca6a6ed08f93cdb1ed
brian.mckenna <brian.mckenna@datto.com>
2023-06-15 15:04:41 +0100

    create context from DbContext

---
+++ API/Models/EmployeeSkillsContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models;
+
+public class EmployeeSkillsContext: DbContext
+{
+    public EmployeeSkillsContext(DbContextOptions<EmployeeSkillsContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Employee>? Employees { get; set; }
+    
+    public DbSet<User>? Users { get; set; }
+    
+    public DbSet<SkillLevel>? SkillLevels { get; set; }
+}
