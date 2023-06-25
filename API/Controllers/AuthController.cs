0ac776bf2c37bbda5ff4c8f65371c7c87f8e07e4
Brian McKenna <D00197352@student.dkit.ie>
2023-06-25 19:08:52 +0100

    added refresh token code but not sure to store refresh token in local storage or not

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.InteropServices.JavaScript;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using API.DTO;
 using API.Interfaces;
@@ -63,10 +65,67 @@ public class AuthController: ControllerBase
         {
             return Unauthorized();
         }
+
+        var jwtToken = GenerateJwtToken(user);
+
+        var refreshToken = GenerateRefreshToken();
         
-        return Ok(GenerateJwtToken(user));
+        SetRefreshTokenToCookie(refreshToken);
+        
+        return Ok(jwtToken);
+    }
+
+    [HttpPost("refresh-token")]
+    public async Task<IActionResult> RefreshToken()
+    {
+        var refreshToken = Request.Cookies["refreshToken"];
+
+        if (!user.RefreshToken.Equals(refreshToken))
+        {
+            return Unauthorized("Invalid refresh token");
+        }
+        else if (user.TokenExpires < DateTime.Now)
+        {
+            return Unauthorized("Token has expired");
+        }
+
+        var token = GenerateJwtToken(user);
+
+        var newRefreshToken = GenerateRefreshToken();
+        SetRefreshTokenToCookie(newRefreshToken);
+
+        return Ok(token);
     }
     
+    private RefreshToken GenerateRefreshToken()
+    {
+        var refreshToken = new RefreshToken
+        {
+            Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+            Expires = DateTime.Now.AddDays(1),
+        };
+
+        return refreshToken;
+    }
+    
+    private void SetRefreshTokenToCookie(RefreshToken refreshToken)
+    {
+        var cookieOptions = new CookieOptions
+        {
+            HttpOnly = true, // prevents client-side scripts accessing the data
+            Expires = refreshToken.Expires
+        };
+        
+        Response.Cookies.Append("refreshToken", refreshToken.Token, cookieOptions);
+        
+        // below needs to be changed as tutorial is saving in memory - he recommended saving in db but 
+        var user = new User();
+
+        user.RefreshToken = refreshToken.Token;
+        user.TokenCreated = refreshToken.CreatedDate;
+        user.TokenExpires = refreshToken.Expires;
+    }
+
     private string GenerateJwtToken(User user)
     {
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:JWT_Token").Value!));
@@ -81,7 +140,7 @@ public class AuthController: ControllerBase
 
         var token = new JwtSecurityToken(
             claims: claims,
-            expires: DateTime.UtcNow.AddHours(2),
+            expires: DateTime.UtcNow.AddMinutes(15),
             signingCredentials: credentials
         );
 
@@ -91,5 +150,4 @@ public class AuthController: ControllerBase
 
         return tokenString;
     }
-
 }
