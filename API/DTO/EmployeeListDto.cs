b33198aeb64198cd980a694ab2924c86595a4ead
brian.mckenna <brian.mckenna@datto.com>
2023-06-29 17:20:29 +0100

    employee list dto

---
+++ API/DTO/EmployeeListDto.cs
@@ -0,0 +1,22 @@
+﻿using API.Models;
+
+namespace API.DTO;
+
+public class EmployeeListDto
+{
+    public string? Id { get; set; }
+    
+    public string? FirstName { get; set; }
+    
+    public string? LastName { get; set; }
+    
+    public DateTime? Dob { get; set; }
+    
+    public string? Email { get; set; }
+
+    public List<SkillLevel> SkillLevels { get; set; }
+
+    public bool IsActive { get; set; } = false;
+
+    public int Age { get; set; }
+}
