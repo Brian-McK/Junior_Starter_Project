ec43a60f68306b2b6a998c9b9c8c6bd263f1efdb
brian.mckenna <brian.mckenna@datto.com>
2023-07-07 16:37:26 +0100

    mid auth unit tests push, issue with cookies context

---
+++ API/Tests/MockData/AuthMockData.cs
@@ -0,0 +1,74 @@
+﻿using API.DTO;
+using API.Models;
+
+namespace API.Tests.MockData;
+
+public class AuthMockData
+{
+    public static User GetUser()
+    {
+        return new User
+        {
+            Id = "64a68656fc13ae0e44beef3f",
+            Username = "admintest",
+            PasswordHash = GeneratePasswordHash("jimmy")
+        };
+    }
+    
+    public static UserReqDto GetUserWithReq()
+    {
+        return new UserReqDto
+        {
+            Username = "admintest",
+            Password = "jimmy"
+        };
+    }
+    
+    public static User GetUserDoesntExist()
+    {
+        return new User
+        {
+            Id = "64a68656fc13ae0344beef1f",
+            Username = "IDontExist",
+            PasswordHash = GeneratePasswordHash("IDontExist")
+        };
+    }
+    
+    public static List<User> GetUsers()
+    {
+        return new List<User>
+        {
+            new()
+            {
+                Id = "64a68656fc13ae0e44beef7f",
+                Username = "Brian",
+                PasswordHash = GeneratePasswordHash("Brian")
+            },
+            new()
+            {
+                Id = "64a68656fc13ae0e44beef84",
+                Username = "Jimmy",
+                PasswordHash = GeneratePasswordHash("Jimmy")
+            },
+            new()
+            {
+                Id = "64a68656fc13ae0e44beef89",
+                Username = "Mary",
+                PasswordHash = GeneratePasswordHash("Mary")
+            },
+            new()
+            {
+                Id = "64a68656fc13ae0e44beef3f",
+                Username = "admintest",
+                PasswordHash = GeneratePasswordHash("jimmy")
+            }
+        };
+    }
+    
+    private static string? GeneratePasswordHash(string password)
+    {
+        var hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+        
+        return hashedPassword;
+    }
+}
---
+++ API/Tests/TestAuthController.cs
@@ -0,0 +1,126 @@
+﻿using API.Controllers;
+using API.DTO;
+using API.Interfaces;
+using API.Models;
+using API.Tests.MockData;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using IConfiguration = Castle.Core.Configuration.IConfiguration;
+
+namespace API.Tests;
+
+public class TestAuthController
+{
+    private readonly Mock<IEmployeeSkillLevelService> _mockEmployeeSkillLevelService;
+    private readonly IConfigurationRoot _configuration;
+    private readonly Mock<HttpResponse> _httpResponse;
+
+    public TestAuthController()
+    {
+        _mockEmployeeSkillLevelService = new Mock<IEmployeeSkillLevelService>();
+
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(@"appsettings.json", false, false)
+            .AddEnvironmentVariables()
+            .Build();
+        
+        _httpResponse = new Mock<HttpResponse>();
+        
+        var cookies = new Mock<IResponseCookies>();
+        _httpResponse.SetupGet(r => r.Cookies).Returns(cookies.Object);
+        
+        var httpContextMock = new Mock<HttpContext>();
+        httpContextMock.SetupGet(c => c.Response).Returns(_httpResponse.Object);
+        
+        var controllerContext = new ControllerContext
+        {
+            HttpContext = httpContextMock.Object
+        };
+
+        // Create the AuthController instance with the mocked dependencies
+        var controller = new AuthController(_mockEmployeeSkillLevelService.Object, _configuration)
+        {
+            ControllerContext = controllerContext
+        };
+    }
+
+    [Fact]
+    public async Task RegisterUser_ReturnsBadRequest_WhenUserAlreadyExists()
+    {
+        var controller = new AuthController(_mockEmployeeSkillLevelService.Object, null);
+
+        var userThatExists = AuthMockData.GetUser();
+        
+        var registerUserDetails = new UserReqDto
+        {
+            Username = userThatExists.Username,
+            Password = userThatExists.PasswordHash
+        };
+        
+        var existingUser = AuthMockData.GetUsers().Find(u => u.Username == userThatExists.Username);
+        
+        _mockEmployeeSkillLevelService.Setup(service => service.GetUserByUsernameAsync(registerUserDetails.Username!))!
+            .ReturnsAsync(existingUser);
+        
+        var result = await controller.RegisterUser(registerUserDetails);
+        
+        Assert.IsType<BadRequestResult>(result);
+    }
+    
+    [Fact]
+    public async Task RegisterUser_WhenUserDoesNotExist_ReturnsOkResult()
+    {
+        var controller = new AuthController(_mockEmployeeSkillLevelService.Object, _configuration);
+
+        var userThatDoesntExist = AuthMockData.GetUserDoesntExist();
+        
+        var registerUserDetails = new UserReqDto
+        {
+            Username = userThatDoesntExist.Username,
+            Password = userThatDoesntExist.PasswordHash
+        };
+        
+        var existingUser = AuthMockData.GetUsers().Find(u => u.Username == registerUserDetails.Username);
+        
+        _mockEmployeeSkillLevelService.Setup(s => s.GetUserByUsernameAsync(registerUserDetails.Username!))!
+            .ReturnsAsync(existingUser);
+        
+        var result = await controller.RegisterUser(registerUserDetails);
+        
+        Assert.IsType<OkObjectResult>(result);
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.IsType<User>(okResult.Value);
+        var user = Assert.IsType<User>(okResult.Value);
+        Assert.Equal(registerUserDetails.Username, user.Username);
+        Assert.True(BCrypt.Net.BCrypt.Verify(registerUserDetails.Password, user.PasswordHash));
+    }
+    
+    [Fact]
+    public async Task Authenticate_WithValidCredentials_ReturnsOkResult()
+    {
+        var controller = new AuthController(_mockEmployeeSkillLevelService.Object, _configuration);
+
+        var userThatExists = AuthMockData.GetUser();
+        
+        var loginDetails = new UserReqDto
+        {
+            Username = userThatExists.Username,
+            Password = "jimmy"
+        };
+
+        _mockEmployeeSkillLevelService.Setup(s => s.GetUserByUsernameAsync(loginDetails.Username!))
+            .ReturnsAsync(userThatExists);
+        
+        var result = await controller.Authenticate(loginDetails);
+        
+        Assert.IsType<OkObjectResult>(result);
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.IsType<AuthResponse>(okResult.Value);
+        var authResponse = Assert.IsType<AuthResponse>(okResult.Value);
+        Assert.Equal(loginDetails.Username, authResponse.Username);
+        Assert.NotNull(authResponse.JwtToken);
+        Assert.NotNull(authResponse.RefreshToken);
+    }
+}
